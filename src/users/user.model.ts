394144476bc9edf1effe8cbda07c59909010268c
YaroshUlad <yarosh.uladzimir@gmail.com>
2023-03-29 19:42:58 +0300

    feat: add users module

---
+++ src/users/user.model.ts
@@ -0,0 +1,5 @@
+interface UserModel {
+  userId: number;
+  username: string;
+  password: string;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  private readonly users: UserModel[] = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
+
+  async findOne(username: string): Promise<UserModel | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+
+  async getAll(): Promise<UserModel[]> {
+    return this.users ? this.users : [];
+  }
+}
