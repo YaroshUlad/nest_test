785093b86823857699db5183b12fcd4c511fff2d
YaroshUlad <yarosh.uladzimir@gmail.com>
2023-03-31 17:07:50 +0300

    feat: add friends module, controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { DeedsModule } from './deeds/deeds.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FriendsModule } from './friends/friends.module';
 
-const uri = 'mongodb+srv://deeds:root@deeds.dg7fx9g.mongodb.net/test';
+const MONGO_URI = 'mongodb+srv://deeds:root@deeds.dg7fx9g.mongodb.net/test';
 
 @Module({
   imports: [
@@ -17,7 +17,7 @@ const uri = 'mongodb+srv://deeds:root@deeds.dg7fx9g.mongodb.net/test';
     FriendsModule,
     MongooseModule.forRootAsync({
       useFactory: () => ({
-        uri,
+        MONGO_URI,
       }),
     }),
   ],
